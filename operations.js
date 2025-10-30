83c67daa2c806c442481ae9cfaa821fdb8b29d04
mgcinalukholwethu-cloud <mgcinalukholwethu@gmail.com>
2025-10-30 15:10:17 +0200

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,21 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 10;
+
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 5;
+
+// TODO: Convert number1 to a string and store it in a new variable
+let number1Str = number1.toString();
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+
+// TODO: Print all results to the console
+console.log("number1:", number1);
+console.log("number2:", number2);
+console.log("number1 as string:", number1Str);
+console.log("sum:", sum);
+console.log("product:", product);
