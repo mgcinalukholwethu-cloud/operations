36c64c3d5d95e8848e610e5720ece65e3d634bfc
mgcinalukholwethu-cloud <mgcinalukholwethu@gmail.com>
2025-10-31 15:32:40 +0200

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,16 @@
+// create a variable called score and give it a value between 0 and 100
+let score = 75;
+
+// Create a variable called passingGrade and set it to 60
+let passingGrade = 60;
+
+// Check for a perfect score first
+if (score === 100) {
+    console.log("Perfect Score!");
+} else if (score >= passingGrade) {
+    // print passed
+    console.log("Passed");
+} else {
+    // print failed
+    console.log("Failed");
+}
